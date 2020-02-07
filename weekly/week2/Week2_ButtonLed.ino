c8c2b5bb98d068c8996849c46dddc9931332de1d
Arnold Martin <arnie.martin@gmail.com>
2020-02-07 11:24:41 -0800

    file re-org

---
+++ weekly/week2/Week2_ButtonLed.ino
@@ -0,0 +1,31 @@
+//global varibles
+int ledPin = 13;  //ledPin
+int delayTime = 1000; //delay time for blinking
+int buttonPin = 2;  //buttonPin
+
+void setup(){
+  //initialize the serial port for debug and testing
+  Serial.begin(9600);
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop(){
+  //check the state of the button
+  int buttonRead = digitalRead(buttonPin);
+
+  //print the value buttonRead
+  Serial.println(buttonRead);
+
+  //print something to the monitor if the button is pushed
+  //button is 0 when pushed
+  if(buttonRead == 0) {
+    Serial.println("Hello World!");
+  }
+  
+  digitalWrite(ledPin, HIGH);
+  delay(delayTime);
+  digitalWrite(ledPin, LOW);
+  delay(delayTime);
+}
